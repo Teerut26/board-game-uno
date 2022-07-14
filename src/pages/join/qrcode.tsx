b41dbea52b1c884278ef01331c5497ff1f1f0177
Teerut26 <teerutsr@gmail.com>
2022-07-14 21:11:43 +0700

    fix : qr code reader

--- src/pages/join/qrcode.tsx
+++ src/pages/join/qrcode.tsx
@@ -1,20 +1,34 @@
-import React from "react";
-import { QrReader } from "react-qr-reader";
+import React, { useEffect, useRef } from "react";
+import QrScanner from "qr-scanner";
 
 interface Props {}
 
 const QrCodeScan: React.FC<Props> = () => {
+    const videoEm = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        const qrScanner = new QrScanner(videoEm.current!, (result) =>
+            handleScan(result),
+            {
+                highlightScanRegion: true,
+            }
+        );
+        qrScanner.start();
+        return () => {
+            qrScanner.stop();
+            qrScanner.destroy();
+        };
+    }, []);
+
+    const handleScan = (result: any) => {
+        console.log("decoded qr code:", result);
+    };
+
     return (
         <div className="absolute top-0 bottom-0  right-0 left-0 flex flex-col justify-center items-center gap-3 p-3">
             <div className="flex flex-col">
                 <div className="max-w-[20rem]">
-                    <QrReader
-                        onResult={(result, error) => {
-                            console.log(result);
-                        }}
-                        videoStyle={{ width: "100%" }}
-                        constraints={{}}
-                    />
+                    <video ref={videoEm}></video>
                 </div>
                 <div></div>
             </div>
