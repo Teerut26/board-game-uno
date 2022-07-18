72677fa2b79c5ac963714bda8fc0b82140d65168
Teerut26 <teerutsr@gmail.com>
2022-07-18 14:10:22 +0700

    clean : delete func getoldplayer

--- src/pages/join/text.tsx
+++ src/pages/join/text.tsx
@@ -47,15 +47,6 @@ const Text: React.FC<Props> = () => {
         } as PlayerInterface);
     };
 
-    const getOldPlayers = () => {
-        return new Promise((resolve, reject) => {
-            const roomRef = ref(database, "/rooms/" + RoomCode + "/players/");
-            onValue(roomRef, (snapshot) => {
-                resolve(snapshot.val());
-            });
-        });
-    };
-
     return (
         <div className="absolute top-0 bottom-0  right-0 left-0 flex flex-col justify-center items-center gap-3 p-3">
             <div className="flex flex-col gap-3">
